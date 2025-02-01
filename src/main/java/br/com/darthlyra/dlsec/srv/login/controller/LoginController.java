e3af932b7bea0f362b9472ff2a919a9dbe0dcd54
vkaczmarzykly <vkaczmarzykly@gmail.com>
2025-01-31 22:49:02 -0300

    feat(controller): Criação básica da controller.

---
+++ src/main/java/br/com/darthlyra/dlsec/srv/login/controller/LoginController.java
@@ -0,0 +1,18 @@
+package br.com.darthlyra.dlsec.srv.login.controller;
+
+import br.com.darthlyra.dlsec.srv.login.dto.DadosAutenticacao;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dlsec-srv-login")
+public class LoginController {
+
+    @PostMapping("/v1/token-funcionario")
+    public ResponseEntity tokenFuncionario(@RequestBody DadosAutenticacao dados) {
+        return ResponseEntity.ok("teste");
+    }
+}
